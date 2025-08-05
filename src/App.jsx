b5d2b42fe29e63a8c9db0881a22c86bddfb4f2d8
Dusked <jazzlyntan2000me@gmail.com>
2025-08-05 11:59:24 +0200

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 
 import StickerCanvas from "./components/StickerCanvas";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
 
   return (
     <>
       <StickerCanvas />
+      <Analytics />
     </>
   )
 }
